c57c7d643823f9c86cd00703bdfd6128219df868
jiswin-varghese <jiswinlogin@gmail.com>
2026-06-29 20:02:10 +0530

    Added physical constants module

---
+++ src/gwmemory/constants.py
@@ -0,0 +1,30 @@
+"""
+constants.py
+============
+
+Physical constants used throughout GWMemoryLab.
+
+Units:
+    SI Units
+
+References:
+    CODATA 2018 recommended values
+"""
+
+# Gravitational constant
+G = 6.67430e-11          # m^3 kg^-1 s^-2
+
+# Speed of light
+c = 2.99792458e8         # m s^-1
+
+# Solar mass
+M_sun = 1.98847e30       # kg
+
+# Parsec
+pc = 3.085677581491367e16    # m
+
+# Megaparsec
+Mpc = 1.0e6 * pc
+
+# Pi
+pi = 3.141592653589793
---
+++ test_constants.py
@@ -0,0 +1,11 @@
+from src.gwmemory.constants import *
+
+print("Testing GWMemoryLab constants")
+print("--------------------------------")
+
+print("G =", G)
+print("c =", c)
+print("Solar Mass =", M_sun)
+print("Parsec =", pc)
+print("Megaparsec =", Mpc)
+print("pi =", pi)
